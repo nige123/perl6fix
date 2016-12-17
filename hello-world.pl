1ac453a04548e4b919738339b7835a10f5349603
Nigel Hamilton <nige@123.do>
2016-12-17 16:53:03 +0000

    First version.

---
+++ hello-world.pl
@@ -0,0 +1,14 @@
+#----------------------------------------------------------
+#
+# Welcome to a demo of perl6fix
+#
+# - this Perl 6 program has numerous syntax errors
+# - let's use perl6fix to fix them
+#
+#----------------------------------------------------------
+
+say 'Hello';
+say 'World';
+
+my $phrase  = 'hello ' ~ 'world';
+my $match   = rx/hello/;
